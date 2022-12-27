6b4c151cac72cd2d474ef8ee9b6f4d8d5387d71f
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2022-12-27 14:47:15 -0300

    game creation from client

--- app/src/components/CreateGame/index.tsx
+++ app/src/components/CreateGame/index.tsx
@@ -1,62 +1,74 @@
 import { FC, useEffect, useState } from "react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { useAnchorWallet } from "@solana/wallet-adapter-react";
-import { PublicKey } from "@solana/web3.js"
-import * as web3 from "@solana/web3.js"
+import { PublicKey } from "@solana/web3.js";
+import * as web3 from "@solana/web3.js";
 import * as anchor from "@project-serum/anchor";
-import { useConnection } from "@solana/wallet-adapter-react"
+import { useConnection } from "@solana/wallet-adapter-react";
 
 import { useProgram } from "../../utils/useProgram";
 
 const endpoint = "http://localhost:8899";
 const connection = new anchor.web3.Connection(endpoint);
 
-export const CreateGame: FC = ({ }) => {
-  //const { connection } = useConnection()
+export const CreateGame: FC = ({}) => {
   const wallet = useAnchorWallet();
   const { program } = useProgram({ connection, wallet });
+  const [player2, setPlayer2] = useState("");
+  const [name, setName] = useState("")
+  const handleClick = () => {
+    if (program) {
+      (async () => {
+        const player2Key = new PublicKey(player2);
+        const [gamePublicKey] = web3.PublicKey.findProgramAddressSync(
+          [Buffer.from("game"), Buffer.from(name)],
+          program.programId
+        );
 
-  const handleClick = ()=>{
-    if (program){
-    (async () =>{
-
-
-  const player1 = new PublicKey("GgZqidq5shJkSZmejx92RTMz2Ti82VxXRdxtKjFKLntC")
-  const player2 = new PublicKey("GgZqidq5shJkSZmejx92RTMz2Ti82VxXRdxtKjFKLntC")
-  const name = "test"
-  
-    const [gamePublicKey] = web3.PublicKey.findProgramAddressSync(
-      [Buffer.from("game"), Buffer.from(name),],
-      program.programId,
-    )
-  
-  
-      const tx = await program.methods.createGame(
-        name,
-        [player1, player2]
-      ).accounts({
-        game: gamePublicKey,
-        payer: wallet?.publicKey,
-        systemProgram: web3.SystemProgram.programId,
-      })
-        .rpc();
-        console.log(`https://explorer.solana.com/tx/${tx}?cluster=custom&customUrl=http://localhost:8899`);
-
-    })()
-  }
- 
-  }
+        const tx = await program.methods
+          .createGame(name, [wallet.publicKey, player2Key])
+          .accounts({
+            game: gamePublicKey,
+            payer: wallet?.publicKey,
+            systemProgram: web3.SystemProgram.programId,
+          })
+          .rpc();
+        console.log(
+          `https://explorer.solana.com/tx/${tx}?cluster=custom&customUrl=http://localhost:8899`
+        );
+      })();
+    }
+  };
   return (
     <>
       <nav className="flex justify-between items-center px-16 py-4 bg-black">
         <WalletMultiButton />
       </nav>
-      {!wallet ? (
-        <p> no wallet </p>
-      ) : (
-        <button type="button" onClick={handleClick}> wallet </button>
-      )}
+      <div className="flex">
+        {!wallet ? (
+          <p> no wallet </p>
+        ) : (
+          <>
+            <input
+              placeholder="Gave your game a name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              placeholder="Copy your friends wallet here"
+              value={player2}
+              onChange={(e) => setPlayer2(e.target.value)}
+            />
+            <button
+              type="button"
+              onClick={player2 && handleClick}
+              className="bg-purple-700 text-white font-extrabold p-2 px-4"
+            >
+              Create Game!
+            </button>
+          </>
+        )}
+      </div>
     </>
   );
 };
-
